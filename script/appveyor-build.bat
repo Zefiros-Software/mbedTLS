3aa307ae52f1207c441abb4834cd72fb6241cabe
PaulVisscher <paul.pev@hotmail.com>
2016-09-08 12:31:46 +0200

    Remove scripts

--- script/appveyor-build.bat
+++
@@ -1,7 +0,0 @@
-cd test/
-premake5 install-package --allow-install --allow-module  || exit /b 1
-premake5 vs2015 || exit /b 1
-
-msbuild Slacking.sln /property:Configuration=Test /property:Platform=x64 || exit /b 1
-
-.\bin\Test\Slacking.exe || exit /b 1
