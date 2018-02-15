360bb3cd6b2403071480f83abde6e7e7e1abdf81
PaulVisscher <paul.pev@hotmail.com>
2018-02-15 19:29:29 +0100

    Update to zpm2

--- script/appveyor-build.bat
+++
@@ -1,7 +0,0 @@
-cd test/
-zpm install-package --allow-install --allow-module  || exit /b 1
-zpm vs2015 || exit /b 1
-
-msbuild mbedtls.sln /property:Configuration=Test /property:Platform=Win32 || exit /b 1
-
-.\bin\Test\mbedTLS.exe --help || exit /b 1
